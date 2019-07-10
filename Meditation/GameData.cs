4377dec44a2c705c7292dfd3dc8c57aa8c9e6731
James McDowell <wejrox@users.noreply.github.com>
2019-07-10 18:30:50 +1000

    Added documentation and stat regen, refactored.
    Added some documentation so that it's easier for other people to contribute in future.
    Added stat regen for health, mana and stamina which is set up the same as burn regen.
    Refactored some of the code to be easier to read and follow.

--- Meditation/GameData.cs
+++ Meditation/GameData.cs
@@ -7,8 +7,14 @@ namespace Meditation
 {
     public class GameData
     {
+        // Burned stats.
         public float BurntStaminaRegen;
         public float BurntHealthRegen;
         public float BurntManaRegen;
+
+        // Current stats.
+        public float CurrentStaminaRegen;
+        public float CurrentHealthRegen;
+        public float CurrentManaRegen;
     }
 }
